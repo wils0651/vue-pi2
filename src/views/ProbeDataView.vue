55fc527d8fac4897405b9414d6d71e0e8d4c1f3f
Tim Wilson <tim@tim.com>
2025-01-29 21:12:50 -0700

    basic chart functionality

--- src/views/ProbeDataView.vue
+++ src/views/ProbeDataView.vue
@@ -3,87 +3,72 @@
     <h1>Probe Data</h1>
   </header>
   <main>
-    <Scatter :data="$data" :options="options">Data not available</Scatter>
+    <WaitCursor :busy="isBusy" msg="Loading Data...."></WaitCursor>
+    <Scatter :data="{ datasets: [{ label: label, data: probeData, backgroundColor: backgroundColor }] }"
+      :options="chartOptions">Data not available</Scatter>
   </main>
 
 </template>
 
-<script>
+<script setup>
+import { onMounted, ref } from "vue";
 import { Scatter } from 'vue-chartjs'
 import { Chart as ChartJS, Title, Tooltip, Legend, LineElement, CategoryScale, LinearScale, PointElement } from 'chart.js'
+import axios from "axios";
+import WaitCursor from "@/components/WaitCursor.vue";
 
 ChartJS.register(Title, Tooltip, Legend, LineElement, CategoryScale, LinearScale, PointElement)
 
-export default {
-  components: {
-    Scatter
+const isBusy = ref(false);
+const probeData = ref([]);
+
+const label = 'Probe 1 Temperature';
+const backgroundColor = 'rgba(75, 192, 192, 0.6)';
+const chartOptions = {
+  responsive: true,
+  plugins: {
+    legend: {
+      display: false,
+    },
   },
-  data() {
-    return {
-      datasets: [
-        {
-          label: 'Scatter Dataset 1',
-          fill: false,
-          borderColor: '#f87979',
-          backgroundColor: '#f87979',
-          data: [
-            {
-              x: -1.5,
-              y: 4
-            },
-            {
-              x: -1.2,
-              y: 1
-            },
-            {
-              x: 0.3,
-              y: 0
-            },
-            {
-              x: 1.5,
-              y: 1
-            },
-            {
-              x: 1.8,
-              y: 4
-            }
-          ]
-        },
-        {
-          label: 'Scatter Dataset 2',
-          fill: false,
-          borderColor: '#7acbf9',
-          backgroundColor: '#7acbf9',
-          data: [
-            {
-              x: -1.9,
-              y: -4
-            },
-            {
-              x: -1.5,
-              y: -1
-            },
-            {
-              x: 0,
-              y: 1
-            },
-            {
-              x: 1.1,
-              y: -1
-            },
-            {
-              x: 2.2,
-              y: -4
-            }
-          ]
-        }
-      ],
-      options: {
-        responsive: true,
-        maintainAspectRatio: false
-      }
+  scales: {
+    x: {
+      // type: "time",
+      // time: {
+      //   unit: "hour",
+      // },
+      title: {
+        display: true,
+        text: "Time",
+      },
+    },
+    y: {
+      title: {
+        display: true,
+        text: "Temperature (°F)",
+      },
+    },
+  },
+};
+
+const getProbeData = async () => {
+  try {
+    isBusy.value = true;
+    const result = await axios(`http://192.168.1.3/api/ProbeData`);
+    if (result.status === 200) {
+      probeData.value = result.data.map((entry) => ({
+        x: new Date(entry.createdDate), // Convert to Date object for Chart.js time scale
+        y: entry.temperature,
+      }));
     }
-  },
-}
+  } catch (error) {
+    console.log("Failed");
+    console.error(error);
+  } finally {
+    isBusy.value = false
+  }
+};
+
+onMounted(getProbeData)
 
 </script>
