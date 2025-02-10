ef096a977dfed5d57995d4af0175b53ceb04ea96
Tim Wilson <tim@tim.com>
2025-02-09 22:01:05 -0700

    Remove WaitCursor from ProbeDataView

--- src/views/ProbeDataView.vue
+++ src/views/ProbeDataView.vue
@@ -6,9 +6,7 @@
     <div class="container mx-4">
       <Scatter :data="datas" :options="chartOptions">Data not available</Scatter>
     </div>
-    <WaitCursor :busy="isBusy" msg=""></WaitCursor>
   </main>
-
 </template>
 
 <script setup>
@@ -16,18 +14,16 @@ import { onMounted, ref, computed } from "vue";
 import { Scatter } from 'vue-chartjs'
 import { Chart as ChartJS, Title, Tooltip, Legend, LineElement, CategoryScale, LinearScale, PointElement, TimeScale } from 'chart.js'
 import axios from "axios";
-import WaitCursor from "@/components/WaitCursor.vue";
 import 'chartjs-adapter-date-fns';
 import { enUS } from 'date-fns/locale';
 
 ChartJS.register(Title, Tooltip, Legend, LineElement, CategoryScale, LinearScale, PointElement, TimeScale)
 
-const isBusy = ref(false);
 const probeData1 = ref([]);
 const probeData2 = ref([]);
 
-const backgroundColor1 = 'rgba(75, 192, 192, 0.6)';
-const backgroundColor2 = 'rgba(175, 122, 92, 0.8)';
+const backgroundColor1 = 'rgba(52, 235, 103, 0.6)';
+const backgroundColor2 = 'rgba(131, 52, 235, 0.8)';
 
 const datas = computed(() => {
   return {
@@ -41,7 +37,8 @@ const chartOptions = {
   responsive: true,
   plugins: {
     legend: {
-      display: false,
+      display: true,
+      position: 'top',
     },
   },
   scales: {
@@ -72,7 +69,6 @@ const chartOptions = {
 
 const getProbeData = async (probeId) => {
   try {
-    isBusy.value = true;
     const result = await axios(`http://192.168.1.3/api/ProbeData/List/${probeId}`);
     if (result.status === 200) {
       if (probeId === 1) {
@@ -80,9 +76,6 @@ const getProbeData = async (probeId) => {
           x: new Date(entry.createdDate), // Convert to Date object for Chart.js time scale
           y: entry.temperature,
         }));
-        //datas.value.datasets[0].data = probeData1.value;
-
-
       } else if (probeId === 2) {
         probeData2.value = result.data.map((entry) => ({
           x: new Date(entry.createdDate), // Convert to Date object for Chart.js time scale
@@ -95,8 +88,6 @@ const getProbeData = async (probeId) => {
   } catch (error) {
     console.log("Failed");
     console.error(error);
-  } finally {
-    isBusy.value = false
   }
 };
 
@@ -106,7 +97,7 @@ onMounted(async () => {
   setInterval(async () => {
     await getProbeData(1);
     await getProbeData(2);
-  }, 10000);
+  }, 60000);
 })
 
 </script>
