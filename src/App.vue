195e87f01d1c60ab1996a7c1691e4b239ac09ff5
Tim Wilson <tim@tim.com>
2025-09-23 21:35:23 -0600

    remove todoItems from route menu

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
   <header>
     <nav class="bg-blue-600 text-white p-4 shadow-md space-x-4">
       <RouterLink to="/" class="hover:text-gray-200">Home</RouterLink>
-      <RouterLink to="/todoItems" class="hover:text-gray-200">Todo Items</RouterLink>
       <RouterLink to="/messages" class="hover:text-gray-200">Messages</RouterLink>
       <RouterLink to="/probeData" class="hover:text-gray-200">Probe Data</RouterLink>
       <RouterLink to="/temperatureStatistics" class="hover:text-gray-200">Temperature Statistics</RouterLink>
